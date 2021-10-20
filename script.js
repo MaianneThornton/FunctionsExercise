9296fdb67ecf9fd93f606e1dbf224b9b74b4aeaf
MaianneThornton <maianne.thornton@gmail.com>
2021-10-20 11:08:19 -0400

    Pre Bonus Section with notes and other possible answers (in class)

--- script.js
+++ script.js
@@ -1,65 +1,112 @@
 // 1
 function spookyGhost() {
-  console.log(`Boo!!!`);
+  console.log(`Boo!!!`); // Boo!!!
 }
 spookyGhost();
 
+console.log("========");
+
 // 2
-candy = (goal) => {
-  console.log(`Get Candy`);
-  console.log(`Get Candy`);
-  console.log(`Get Candy`);
-  console.log(`Get Candy`);
+function candy(goal) {
+  console.log(goal);
+  console.log(goal);
+  console.log(goal);
+  console.log(goal);
+  //OR
+  // console.log(goal, goal, goal, goal);
+
 }
-candy();
+candy(`Get Candy`);
+/*
+Get Candy
+Get Candy
+Get Candy
+Get Candy
+*/
+
+console.log("========");
 
 // 3
-function werewolf (a, b){
-  if (a === `Full` && b === `Moon`){
-  console.log(`ARH-WOOO!`);
-  } else {
-    console.log(``);
+function werewolf(a, b) {
+  if (a === `Full` && b === `Moon`) {
+    console.log(`ARH-WOOO!`);
   }
-  
+
+  // Using a Ternary
+  // console.log((a === `Full` && b === `Moon`) ? `ARH-WOOO!` : "");
+
 }
-werewolf(`Full`, `Moon`);
-// werewolf();
+werewolf(`Full`, `Moon`); // ARH-WOOO!
+werewolf(`Crescent`, `Moon`); // Does NOT display anything in the terminal/console
+
+
+console.log("========");
+
 
 // 4
-halloween = () => `October 31st`;
-console.log(halloween());
+function halloween() {
+  return `October 31st`;
+}
+console.log(halloween()); // October 31st
+
+console.log("========");
 
 // 5 
-function isEnoughCandy (pieces) {
- if (pieces >= 31) {
-   return true;
- } else if (pieces < 31) {
-   return false;
- }
+function isEnoughCandy(pieces) {
+  if (pieces >= 31) {
+    return true;
+  } else {
+    return false;
+  }
+
+  // USING A TERNARY
+  // return (pieces >= 31 ? true : false)
 };
-// isEnoughCandy(1);
-// isEnoughCandy(31);
+console.log(isEnoughCandy(31)); // true
+console.log(isEnoughCandy(1)); // false
+
+console.log("========");
+
 
 // 6
-// candyCounter = nums => {
-//   ([1, 2, 3]);
-//   for (i = 0; i < num.length; i++){
-//     total = total + num[i];
-// }
-// nums();
+function candyCounter(nums) {
+  let total = 0;
+  for (i = 0; i < nums.length; i++) {
+    total += nums[i];
+  }
+  return total;
 
+  // USING A FOR OF LOOP
+  // let total = 0;
+  // for (number of nums) {
+  //   total += number;
+  // }
+  // return total;
+}
+console.log(candyCounter([1, 2, 3])); // 6
+
+
+console.log("========");
 
 // 7
-function mansion () {
-const hauntedMansion = `Welcome, foolish mortals, to the Haunted Mansion!`
-console.log(hauntedMansion);
+const hauntedMansion = function () {
+  console.log("Welcome, foolish mortals, to the Haunted Mansion!");
 }
-mansion();
+hauntedMansion(); // Welcome, foolish mortals, to the Haunted Mansion!
+
+console.log("========");
 
 // 8
-ghostbusters = () => 
-console.log(`Who You Gonna Call?`);
-ghostbusters();
+ghostbusters = () =>
+  `Who You Gonna Call?`;
+
+// USING PARENTHESIS
+// ghostbusters = () => (
+//   `Who You Gonna Call?`
+//   );
+
+console.log(ghostbusters()); // Who You Gonna Call?
+
 
 // 9
 // QUESTION: If a let or const variable is created/declared inside a function, loop, or conditional can the value of it be accessed outside of the function, loop, or conditional?
@@ -69,15 +116,36 @@ ghostbusters();
 // QUESTION: If a let or const variable is created/declared outside the scope of a function called change and the value of the variable is updated inside the change function, then (assuming the function has been run) could the updated value of the variable be accessed outside of the function
 // YES
 
+
 // 11a
 const costumes = [
-  "Ghost", 
-  "Princess", 
+  "Ghost",
+  "Princess",
   "Pirate"
 ];
 
+console.log("========");
+
 // 11b
-const upperCostumes = costumes.map(costumes => costumes.toUpperCase()); 
-console.log(upperCostumes);
+const upperCostumes = costumes.map(i => i.toUpperCase());
 
+// Using a Declaration Function
+// const upperCostumes = costumes.map(function(i){
+//   return i.toUpperCase();
+// });
 
+console.log(upperCostumes); // [ 'GHOST', 'PRINCESS', 'PIRATE' ]
+
+console.log("========");
+
+// BONUS SECTION
+// 12a
+const stephenKingMovies = [
+  "The Shining",
+  "Christine",
+  "It",
+  "The Mist",
+  "Creepshow",
+  "Pet Cemetary"
+];
+console.log(stephenKingMovies);
